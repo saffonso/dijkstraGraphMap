2432390a53b5e0704bfbc5b7a01155af3b14fc23
Santiago Affonso <83676299+saffonso@users.noreply.github.com>
2026-02-06 13:18:37 -0300

    test

---
+++ build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_final_project_FILE C:/Users/santi/OneDrive/Documentos/dijkstraGraphMap/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/final_project.exe)
+set(__QT_DEPLOY_TARGET_final_project_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_final_project_RUNTIME_DLLS C:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
